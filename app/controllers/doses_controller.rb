18069a23ad256013d572046106d59f42e6166e0d
Cataline Baert <snuf_kin@yahoo.com>
2019-08-08 15:53:18 +0200

    add content to new dose and methods new and create

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,26 @@
+class DosesController < ApplicationController
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    # we need `cocktail_id` to asssociate review with corresponding cocktail
+    @dose.cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.save
+
+    redirect_to cocktail_path(@dose.cocktail)
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id, :cocktail_id)
+  end
+end
