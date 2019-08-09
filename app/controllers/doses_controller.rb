8d93ac9ca62f3c197b2a3cce6827d6602f58e8d0
Cataline Baert <snuf_kin@yahoo.com>
2019-08-09 10:32:40 +0200

    refactor route delete and method destroy

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -8,18 +8,19 @@ class DosesController < ApplicationController
     @dose = Dose.new(dose_params)
     # we need `cocktail_id` to asssociate review with corresponding cocktail
     @dose.cocktail = Cocktail.find(params[:cocktail_id])
-    @dose.save
-
-    redirect_to cocktail_path(@dose.cocktail)
+    if @dose.save
+      redirect_to cocktail_path(@dose.cocktail)
+    else
+      render :new
+    end
   end
 
   def destroy
-    @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.find(params[:id])
-    # raise
+    cocktail = @dose.cocktail
     @dose.destroy
 
-    redirect_to cocktail_path(@cocktail)
+    redirect_to cocktail_path(cocktail)
   end
 
   private
