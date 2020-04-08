1d60c2a7102940123078409e0c3d18e2bbbd05b6
Cataline Baert <snuf_kin@yahoo.com>
2020-04-08 16:25:15 +0200

    define method destroy for cocktails, add button to delete cocktails, add style and modify image transformation

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -20,6 +20,13 @@ class CocktailsController < ApplicationController
     @cocktail = Cocktail.find(params[:id])
   end
 
+  def destroy
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.destroy
+
+    redirect_to root_path
+  end
+
   private
 
   def cocktail_params
